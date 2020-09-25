8a6f90d11c26bd9b41f24fd250ac817d6ebdccbf
Lind, Niclas <niclas.lind@volvocars.com>
2020-09-25 09:09:13 +0200

    removed old and unecceseary things in this LIN class

--- lin.cpp
+++ lin.cpp
@@ -22,168 +22,55 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include "lin.h"
+#include "lin.hpp"
+#include "HardwareSerial.h"
 
-Lin::Lin(LIN_SERIAL &ser, uint8_t TxPin)
-    : serial(ser), txPin(TxPin)
-{
+#include <array>
+
+void Lin::begin() {
+  m_Serial.begin(serialSpd);
 }
 
-void Lin::begin(int speed)
-{
-  serialSpd = speed;
-  serial.begin(serialSpd);
-  serialOn = 1;
 
-  unsigned long int Tbit = 100000 / serialSpd;                    // Not quite in uSec, I'm saving an extra 10 to change a 1.4 (40%) to 14 below...
-  unsigned long int nominalFrameTime = ((34 * Tbit) + 90 * Tbit); // 90 = 10*max # payload bytes + checksum (9).
-  timeout = LIN_TIMEOUT_IN_FRAMES * 14 * nominalFrameTime;        // 14 is the specced addtl 40% space above normal*10 -- the extra 10 is just pulled out of the 1000000 needed to convert to uSec (so that there are no decimal #s).
+void Lin::serialBreak() {
+  constexpr auto LIN_BREAK_DURATION = 15;
+
+  m_Serial.flush();
+  delay(1);
+  m_Serial.end();
+  gpio_reset_pin(GPIO_NUM_4);
+  gpio_matrix_out(m_TxPin, SIG_GPIO_OUT_IDX, false, false);
+
+//  pinMode(m_TxPin, OUTPUT);
+  digitalWrite(m_TxPin, LOW); // Send BREAK
+
+  constexpr auto brakeEnd = (1000000UL / static_cast<unsigned long>(serialSpd));
+  constexpr auto brakeBegin = brakeEnd * LIN_BREAK_DURATION;
+
+  // delayMicroseconds unreliable above 16383 see Arduino man pages
+  delayMicroseconds(brakeBegin);
+
+  digitalWrite(m_TxPin, HIGH); // BREAK delimiter
+
+  m_Serial.begin(serialSpd);
 }
 
-// Generate a BREAK signal (a low signal for longer than a byte) across the serial line
-void Lin::serialBreak(void)
-{
-  if (serialOn)
-  {
-    serial.flush();
-    gpio_matrix_out(txPin, SIG_GPIO_OUT_IDX, false, false);
+
+int Lin::calculateChecksum(const unsigned char data[], uint8_t data_size) {
+  int sum = 0;
+
+  for (int i = 0; i < data_size; i++) {
+    sum = sum + data[i];
   }
 
-  pinMode(txPin, OUTPUT);
-  digitalWrite(txPin, LOW); // Send BREAK
-
-  const unsigned long int brkend = (1000000UL / ((unsigned long int)serialSpd));
-  const unsigned long int brkbegin = brkend * LIN_BREAK_DURATION;
-  if (brkbegin > 16383)
-    delay(brkbegin / 1000); // delayMicroseconds unreliable above 16383 see arduino man pages
-  else
-    delayMicroseconds(brkbegin);
-
-  digitalWrite(txPin, HIGH); // BREAK delimiter
-
-  if (brkend > 16383)
-    delay(brkend / 1000); // delayMicroseconds unreliable above 16383 see arduino man pages
-  else
-    delayMicroseconds(brkend);
-
-  serial.begin(serialSpd);
-  serialOn = 1;
-}
-
-/* Lin defines its checksum as an inverted 8 bit sum with carry */
-uint8_t Lin::dataChecksum(const uint8_t *message, char nBytes, uint16_t sum)
-{
-  while (nBytes-- > 0)
-    sum += *(message++);
-  // Add the carry
-  while (sum >> 8) // In case adding the carry causes another carry
-    sum = (sum & 255) + (sum >> 8);
-  return (~sum);
-}
-
-/* Create the Lin ID parity */
-#define BIT(data, shift) ((addr & (1 << shift)) >> shift)
-uint8_t Lin::addrParity(uint8_t addr)
-{
-  uint8_t p0 = BIT(addr, 0) ^ BIT(addr, 1) ^ BIT(addr, 2) ^ BIT(addr, 4);
-  uint8_t p1 = ~(BIT(addr, 1) ^ BIT(addr, 3) ^ BIT(addr, 4) ^ BIT(addr, 5));
-  return (p0 | (p1 << 1)) << 6;
-}
-
-int Lin::calulateChecksum(unsigned char data[], byte data_size)
-{
-  int suma = 0;
-  for (int i = 0; i < data_size; i++)
-  {
-    suma = suma + data[i];
-  }
-  byte v_checksum = (byte) ~(suma % 0xff);
+  const auto v_checksum = static_cast<uint8_t>(~(sum % 0xffu));
   return v_checksum;
 }
 
 // according to enhanced crc
-boolean Lin::validateChecksum(unsigned char data[], byte data_size)
-{
-  byte checksum = data[data_size - 1];
-  byte v_checksum = calulateChecksum(data, data_size - 1);
+bool Lin::validateChecksum(const unsigned char data[], uint8_t data_size) {
+  uint8_t checksum = data[data_size - 1];
+  uint8_t v_checksum = calculateChecksum(data, data_size - 1);
 
   return checksum == v_checksum;
-}
-
-void Lin::send(uint8_t addr, const uint8_t *message, uint8_t nBytes)
-{
-  uint8_t cksum = dataChecksum(message, 8, addr);
-
-  serial.write(message, 8); // data bytes
-  serial.write(cksum);      // checksum
-  serial.flush();
-}
-
-uint8_t Lin::recv(uint8_t addr, uint8_t *message, uint8_t nBytes, uint8_t proto)
-{
-  uint8_t bytesRcvd = 0;
-  unsigned int timeoutCount = 0;
-  serialBreak(); // Generate the low signal that exceeds 1 char.
-  serial.flush();
-  serial.write(0x55); // Sync byte
-  uint8_t idByte = (addr & 0x3f) | addrParity(addr);
-  serial.write(idByte); // ID byte
-  bytesRcvd = 0xfd;
-  do
-  { // I hear myself
-    while (!serial.available())
-    {
-      delayMicroseconds(100);
-      timeoutCount += 100;
-      if (timeoutCount >= timeout)
-        goto done;
-    }
-  } while (serial.read() != 0x55);
-  bytesRcvd = 0xfe;
-  do
-  {
-    while (!serial.available())
-    {
-      delayMicroseconds(100);
-      timeoutCount += 100;
-      if (timeoutCount >= timeout)
-        goto done;
-    }
-  } while (serial.read() != idByte);
-
-  bytesRcvd = 0;
-  for (uint8_t i = 0; i < nBytes; i++)
-  {
-    // This while loop strategy does not take into account the added time for the logic.  So the actual timeout will be slightly longer then written here.
-    while (!serial.available())
-    {
-      delayMicroseconds(100);
-      timeoutCount += 100;
-      if (timeoutCount >= timeout)
-        goto done;
-    }
-    message[i] = serial.read();
-    bytesRcvd++;
-  }
-  while (!serial.available())
-  {
-    delayMicroseconds(100);
-    timeoutCount += 100;
-    if (timeoutCount >= timeout)
-      goto done;
-  }
-  if (serial.available())
-  {
-    uint8_t cksum = serial.read();
-    bytesRcvd++;
-    if (proto == 1)
-      idByte = 0; // Don't cksum the ID byte in LIN 1.x
-    if (dataChecksum(message, nBytes, idByte) == cksum)
-      bytesRcvd = 0xff;
-  }
-
-done:
-  serial.flush();
-
-  return bytesRcvd;
 }
